faab65569181d1433ff5aac01dc202e532993361
Henrique Esteves <henriesteves@gmail.com>
2017-11-01 00:11:49 -0200

    Setup app for heroku

--- server/server.js
+++ server/server.js
@@ -8,6 +8,8 @@ var { User } = require('./models/user');
 
 var app = express();
 
+const port = process.env.PORT || 3000;
+
 app.use(bodyParser.json());
 
 // Create todo
@@ -43,7 +45,7 @@ app.get('/todos/:id', (req, res) => {
 
     Todo.findById(id).then((todo) => {
         if (!todo) {
-            return res.status(404).send()
+            return res.status(404).send();
         }
         res.send({ todo });
     }, (e) => {
@@ -51,8 +53,8 @@ app.get('/todos/:id', (req, res) => {
     });
 });
 
-app.listen(3000, () => {
-    console.log('Started on port 3000')
+app.listen(port, () => {
+    console.log('Started on port', port);
 });
 
 module.exports = {
